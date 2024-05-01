be233a92de93aa37ef19525dfe8dd4f55aed1108
jhmarketing <168642398+jhmarketing@users.noreply.github.com>
2024-05-01 09:10:19 -0400

    Update settings.py

--- appfitness/settings.py
+++ appfitness/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
@@ -131,7 +131,7 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
